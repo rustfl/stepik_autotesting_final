e8409de6630b5bf4515f61860e0c77e5948a9498
Rustem Akhmetganiev <sepen@tuta.io>
2021-03-02 10:13:42 +0500

    Added is_element_present method in BasePage class

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,3 +5,6 @@ from selenium.webdriver.common.by import By
 class MainPage(BasePage):
     def go_to_login_page(self):
         self.browser.find_element(By.CSS_SELECTOR, "#login_link").click()
+
+    def should_bo_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
